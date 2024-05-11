7a5efbad0330fb50937f79c0133f356cad64e567
FelipeSarco17 <sarcofelipe456@gmail.com>
2024-05-11 03:41:05 -0300

    Finalizacion de funcionalidad boton carrito homepage, y boton restar en carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,8 +1,136 @@
+let carrito = leerCarrito();
+let listaCarrito = document.querySelector("#listadoCarrito");
+let carritoVacio = document.querySelector("#carritoVacio");
+
+
+if(carrito.length!=0){
+    carritoVacio.style.display = "none";
+}
+
+carrito.forEach((productoCarrito)=>{
+
+    listaCarrito.innerHTML += `
+
+    <article class="productoCarrito">
+        <div class="textoProductoCarrito">
+            <img class="imgProductoCarrito" src="${productoCarrito.imagen}" alt="">
+   
+            <h4 class="nombreProducto">${productoCarrito.nombre}</h4>
+         </div>
+
+        <div class="botonesCarrito">
+        
+            <button class="restarProducto" onclick="eliminarProductoCarrito(${productoCarrito.id})">-</button>
+            <p class="cantidadProducto">${productoCarrito.cantidadEnCarrito}</p>
+            <button class="sumarProducto" onclick="agregarProductoCarrito(${productoCarrito.id})">+</button>
+        
+            <p class="precioProductoLista">$${productoCarrito.precio*productoCarrito.cantidadEnCarrito}</p>
+        </div>
+
+    </article>
+    
+    
+    `;
+
+});
+
+
 function leerCarrito(){
     let carrito = localStorage.getItem("carrito");
     return JSON.parse(carrito);
 }
 
 
-let carrito = leerCarrito();
+
+function cargarCarrito(data){
+    localStorage.setItem("carrito",JSON.stringify(data));
+}
+
+
+function eliminarProductoCarrito(idProducto){
+    
+    let productoEliminar;
+    let indice;
+    let contadorCarrito = document.querySelector("#contadorCarrito");
+
+
+    carrito.forEach((productoCarrito,index)=>{
+
+        if(productoCarrito.id==idProducto){
+            
+            productoEliminar = productoCarrito;
+            indice = index;
+        }
+
+    });
+    
+    if(productoEliminar.cantidadEnCarrito == 1){
+        
+        carrito.splice(indice,1);
+        
+    }
+    else{
+        productoEliminar.cantidadEnCarrito--;
+        carrito[indice] = productoEliminar;
+    }
+
+    contadorCarrito.innerText = `${carrito.length}`;
+
+    cargarCarrito(carrito);
+    actualizarLista();
+    
+
+}
+
+
+
+function actualizarLista(){
+
+    listaCarrito.innerHTML = "";
+
+    
+    if(carrito.length == 0){
+        
+        listaCarrito.innerHTML = `
+        <div id="carritoVacio">
+            <img src="../img/shoppingBag.svg" alt="logoBolsaCompras">
+            <h4>¡Empieza un carrito de compras!</h4>
+            <a href="../index.html" id="botonDescubriProductos">Descubrir productos</a>
+        </div>`;    
+        
+    }
+    else{
+        carrito.forEach((productoCarrito)=>{
+
+            listaCarrito.innerHTML += `
+        
+            <article class="productoCarrito">
+                <div class="textoProductoCarrito">
+                    <img class="imgProductoCarrito" src="${productoCarrito.imagen}" alt="">
+           
+                    <h4 class="nombreProducto">${productoCarrito.nombre}</h4>
+                 </div>
+        
+                <div class="botonesCarrito">
+                
+                    <button class="restarProducto" onclick="eliminarProductoCarrito(${productoCarrito.id})">-</button>
+                    <p class="cantidadProducto">${productoCarrito.cantidadEnCarrito}</p>
+                    <button class="sumarProducto" onclick="agregarProductoCarrito(${productoCarrito.id})">+</button>
+                
+                    <p class="precioProductoLista">$${productoCarrito.precio*productoCarrito.cantidadEnCarrito}</p>
+                </div>
+        
+            </article>
+            
+            
+            `;
+        
+        });
+    }
+    
+}
+
+
+
+
 
--- js/homepage.js
+++ js/homepage.js
@@ -2,6 +2,7 @@ let productosHomePage = [];
 let favoritosHomePage = [];
 let carritoHomePage = [];
 
+
 fetch("./js/productos.json")
 .then(response => response.json())
 .then((data)=>{
@@ -23,8 +24,8 @@ fetch("./js/productos.json")
                         $${producto.precio}
                     </p>
 
-                    <button href="" class="botonFavoritos" onclick="añadirProductoFavoritos(${producto.id})"><img src="./img/heartBlack.svg" alt="LogoFavoritos" class="logoFavCards"></button>
-                    <button href="" class="botonCarritoAñadir" onclick="añadirProductoAlCarrito(${producto.id})"><img src="./img/addCart.svg" alt="CarritoAñadir" ></button>
+                    <button href="" id="botonFavoritoAñadir${producto.id}" class="botonFavoritos" onclick="añadirProductoFavoritos(${producto.id})" ><img src="./img/heartBlack.svg" alt="LogoFavoritos" class="logoFavCards"></button>
+                    <button href="" id="botonCarritoAñadir${producto.id}" class="botonCarritoAñadir" onclick="añadirProductoAlCarrito(${producto.id})"><img src="./img/addCart.svg" alt="CarritoAñadir" ></button>
           
                 </div>
             </div>
@@ -40,20 +41,29 @@ fetch("./js/productos.json")
 .catch((error) => console.error("No se pudo conseguir la data:", error));
 
 window.onload = ()=>{
-    leerCarrito();
-    leerFavoritos();
-    let contadoresFavoritos = document.querySelectorAll("#contadorFavs");
-    contadoresFavoritos.forEach((contadorFavoritos)=>{
+    
+    
+        
+        leerCarrito();
+         
+        let contadoresCarrito = document.querySelectorAll("#contadorCarrito");
+        contadoresCarrito.forEach((contadorCarrito)=>{
+            contadorCarrito.innerText = `${carritoHomePage.length}`;
+        });
+    
+
+    
+        
+        leerFavoritos();
+        let contadoresFavoritos = document.querySelectorAll("#contadorFavs");
+        contadoresFavoritos.forEach((contadorFavoritos)=>{
         contadorFavoritos.innerText = `${favoritosHomePage.length}`;
-    });
-    let contadoresCarrito = document.querySelectorAll("#contadorCarrito");
-    contadoresCarrito.forEach((contadorCarrito)=>{
-        contadorCarrito.innerText = `${carritoHomePage.length}`;
-    });
+        });
+    
 };
 
 
-const botonesFav = document.querySelectorAll(".botonFavoritos");
+
 
 let usuarios = [{email:"felipe@gmail.com",
                  password:"1234",
@@ -70,19 +80,28 @@ let usuarios = [{email:"felipe@gmail.com",
                     telefono:"38123123",
                     domicilio: "bascary 4365"
                 }
-                ];
+];
 
 
 
 // LOCAL STORAGE
 
 function leerCarrito(){
-    carritoHomePage = JSON.parse(localStorage.getItem("carrito"));
+    let carritoLocalStorage = JSON.parse(localStorage.getItem("carrito"));
+    if(carritoLocalStorage){
+        
+        carritoHomePage = carritoLocalStorage;
+    }
+    
 }
 
 function leerFavoritos(){
     
-    favoritosHomePage = JSON.parse(localStorage.getItem("favoritos"));
+    let favoritosLocalStorage = JSON.parse(localStorage.getItem("favoritos")); 
+    if(favoritosLocalStorage){
+        favoritosHomePage = favoritosLocalStorage;
+    }
+    
 }
 
 function cargarFavoritos(data){
@@ -103,40 +122,47 @@ function cargarCarrito(data){
 // BOTON LOGIN
 
 const botonLogin = document.querySelector("#botonModalLogin");
-const pagina = document.getElementsByTagName("title");
 botonLogin.setAttribute("data-bs-dismiss"," ");
 botonLogin.addEventListener("click",(e)=>{
 
     const email = document.querySelector("#inputEmail").value;
     const password = document.querySelector("#inputPass").value;
+    let login = false;
+    
+ 
+    for(let i=0;i<usuarios.length;i++){
 
-    usuarios.forEach((usuario)=>{
-
-        if(usuario.email==email && usuario.password==password){
+        if(usuarios[i].email==email && usuarios[i].password==password){
             let perfil = document.querySelector("#botonLogIn");
-
-                perfil.innerHTML = `<img src="./img/user.svg" alt="LogoUser">${usuario.nombre}`;
-                botonLogin.setAttribute("data-bs-dismiss","modal");
+            botonLogin.setAttribute("data-bs-dismiss","modal");
+            perfil.innerHTML = `<img src="./img/user.svg" alt="LogoUser">${usuarios[i].nombre}`;
             
-        }
-        else{
-
-                let modalBodyLogin = document.querySelector("#modalBodyLogin");
-                modalBodyLogin.innerHTML = `
-                <form action="" id="formLogin">
-
-                <label for="inputEmail"><img src="./img/user.svg" alt="logoUser">Email</label>
-                <input type="text" name="email" id="inputEmail" required>
-                <label for="inputPass"><img src="./img/logoPassword.svg" alt="logoPassword">Contraseña</label>
-                <input type="password" name="pass" id="inputPass" required>
-                </form>
-                <p style="color:red; text-align:center; margin-top:5px;">La contraseña o el correo ingresados son incorrectos</p>
-                <p id="linksModal"><a href="" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">¿No tienes una cuenta?</a> <a href="">¿Olvidaste tu contraseña?</a></p>
-                `;
+            login=true;
             
-        }
+         }
+        else
+        {
 
-    });
+            let modalBodyLogin = document.querySelector("#modalBodyLogin");
+            modalBodyLogin.innerHTML = `
+            <form action="" id="formLogin">
+
+            <label for="inputEmail"><img src="./img/user.svg" alt="logoUser">Email</label>
+            <input type="text" name="email" id="inputEmail" required>
+            <label for="inputPass"><img src="./img/logoPassword.svg" alt="logoPassword">Contraseña</label>
+            <input type="password" name="pass" id="inputPass" required>
+            </form>
+            <p style="color:red; text-align:center; margin-top:5px;">La contraseña o el correo ingresados son incorrectos</p>
+            <p id="linksModal"><a href="" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">¿No tienes una cuenta?</a> <a href="">¿Olvidaste tu contraseña?</a></p>
+            `;
+        
+         }
+
+         if(login) break;
+
+    }
+
+
 
 });
 
@@ -170,24 +196,28 @@ botonRegistro.addEventListener("click",()=>{
 
 // BOTON CARRITO
 
-
-
-
-
 function añadirProductoAlCarrito(idProducto){
 
+    console.log(2);
     let productoCarrito;
     productosHomePage.forEach((producto)=>{
 
-        if(idProducto == producto.id){
+        if(idProducto == producto.id && producto.enCarrito == false){
+            
             productoCarrito = producto;
+        }
+        else{
+            return;
         }
     });
     
+
+    productoCarrito.enCarrito = true;
+    productoCarrito.cantidadEnCarrito = 1;
     carritoHomePage.push(productoCarrito);
     cargarCarrito(carritoHomePage);
     let contadoresCarrito = document.querySelectorAll("#contadorCarrito");
-    console.log(contadoresCarrito);
+   
     contadoresCarrito.forEach((contadorCarrito)=>{
         contadorCarrito.innerText = `${carritoHomePage.length}`;
     });
@@ -195,29 +225,35 @@ function añadirProductoAlCarrito(idProducto){
 
 }
 
+// ****************************
+
+// BOTON FAVORITOS
 
 function añadirProductoFavoritos(idProducto){
 
     let productoFavorito;
     productosHomePage.forEach((producto)=>{
 
-        if(idProducto == producto.id){
+        if(idProducto == producto.id && producto.enFavoritos == false){
+            
             productoFavorito = producto;
+        }
+        else{
+            return;
         }
     });
 
+    productoFavorito.enFavoritos = true;
     favoritosHomePage.push(productoFavorito);
     cargarFavoritos(favoritosHomePage);
     let contadoresFavoritos = document.querySelectorAll("#contadorFavs");
-
     contadoresFavoritos.forEach((contadorFavoritos)=>{
-        console.log(favoritosHomePage.length);
         contadorFavoritos.innerText = `${favoritosHomePage.length}`;
     });
-    
+
 }
 
-
+// ****************************
 
 
 
